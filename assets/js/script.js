2c07455b36e5b630cc377c3ed69f4059570132b7
itzguled <itzguled55337@gmail.com>
2022-02-04 22:38:18 -0600

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var searchBtn = document.querySelector(".btn-primary");
+var searchInputEl = document.querySelector("#city-name");
 
 var theCity = function (city) {
   var apiUrl =
@@ -8,24 +9,32 @@ var theCity = function (city) {
     if (response.ok) {
       response.json().then(function (data) {
         data["city"]["coord"]["lat"];
+
         var lat = data["city"]["coord"]["lat"];
         var lon = data["city"]["coord"]["lon"];
-        theCityOneCall(lat, lon)
       });
     }
   });
 };
-var oneCall = function (lat,lon) {
+var oneCall = function (lat, lon) {
   apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=c1ecab3291362f3fe61c8735f3bd9de6&q`;
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
-        displayCity(data)
+        displayCity(data);
       });
     }
   });
 };
 
-// var displayCity = function {
+function searchButton(event) {
+  event.preventDefault();
+  var city = searchInputEl.value.trim();
+  if (city) {
+    theCity(city);
+  }
+}
 
-// }
+var displayCity = function (data) {};
+
+searchBtn.addEventListener("submit", searchButton);
